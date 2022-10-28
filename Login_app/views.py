0002668d758768e578ebd3baffab40c537767081
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-10-28 21:35:23 +0600

    Register User submit

--- Login_app/views.py
+++ Login_app/views.py
@@ -8,8 +8,31 @@ def index(request):
     return render(request, 'Login_app/index.html', context=diction)
 
 def register(request):
-    user_form = UserForm()
-    user_info_form = UserInfoForm()
+    registered = False
 
-    diction = {'user_form':user_form, 'user_info_form':user_info_form}
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        user_info_form = UserInfoForm(data=request.POST)
+
+        if user_form.is_valid() and user_info_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password) # password hash a convert
+            user.save()
+
+
+            user_info = user_info_form.save(commit=False)
+            user_info.user = user
+
+            if 'profile_pic' in request.FILES:
+                user_info.profile_pic = request.FILES['profile_pic']
+
+            user_info.save()
+
+            registered = True
+
+    else:
+        user_form = UserForm()
+        user_info_form = UserInfoForm()
+
+    diction = {'user_form':user_form, 'user_info_form':user_info_form, 'registered':registered}
     return render(request, 'Login_app/register.html', context=diction)
