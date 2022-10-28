26d7852a5b95053737362afb399ff73dcfc454c2
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-10-28 20:48:44 +0600

    Creating Registration Form

--- Login_app/views.py
+++ Login_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from Login_app.forms import UserForm, UserInfoForm
+
 
 # Create your views here.
 def index(request):
     diction = {}
     return render(request, 'Login_app/index.html', context=diction)
+
+def register(request):
+    user_form = UserForm()
+    user_info_form = UserInfoForm()
+
+    diction = {'user_form':user_form, 'user_info_form':user_info_form}
+    return render(request, 'Login_app/register.html', context=diction)
